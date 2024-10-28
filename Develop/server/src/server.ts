c83cd6a9349f6c59ad28b22fe350ce7296b611fa
Kristen Shields <kristenshields@Kristens-MacBook-Air.local>
2024-10-28 17:57:02 -0600

    added more code

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -7,12 +7,12 @@ dotenv.config();
 import routes from './routes/index.js';
 const app = express();
 const PORT = process.env.PORT || 3001;
-app.use(express.json());
-app.use(routes);
 // Serve static files
 app.use(express.static('../client/dist'));
 // Middleware for parsing JSON
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
 // Connect the routes
 // Start the server
 
